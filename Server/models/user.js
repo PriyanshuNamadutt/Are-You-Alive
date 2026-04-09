36d080a3e99490b864a4b2165ceb2358bfa28c3c
PriyanshuNamadutt <namaduttpriyanshu@gmail.com>
2026-04-10 02:24:53 +0530

    Add files via upload

---
+++ Server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const userschema = new mongoose.Schema({
+
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+
+    lastClick: {
+        type: Date,
+        default: null
+    }
+});
+
+module.exports = mongoose.model( "user", userschema );
