2135d8ef2d1e1e0f6fbeb2ee7ee4e6e9ae637574
PriyanshuNamadutt <namaduttpriyanshu@gmail.com>
2026-04-10 03:21:19 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -17,7 +17,8 @@ mongoose.connect(process.env.Mongo_URI)
 
 app.use("/api", require("./routes/auth") );
 
-const port = 5000;
-app.listen( port, () => {
-    console.log(`app is listening at ${port}` );
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
